a46d016ca594fba54bf670bedb570f1cd7f130af
dev <rajenendr2024@gmail.com>
2024-11-07 10:46:54 +0530

    3 point are straight line

---
+++ python/assignment/7-11-2024/straight_line_prove.py
@@ -0,0 +1,15 @@
+# 9 Given the point (x1,y1), (x2,y2), (x3,y3) WAP to check all the point falls on straight line.
+x1= int(input("Enter x1: "))
+y1= int(input("Enter y1: "))
+x2= int(input("Enter x2: "))
+y2= int(input("Enter y2: "))
+x3= int(input("Enter x3: "))
+y3= int(input("Enter y3: "))
+
+area = 0.5 * ( x1*(y1-y3) + x2*(y3-y1) + x3*(y1-y2) )
+
+
+if area == 0:
+    print("Above points are colliner or lies on a straight line")
+else:
+    print("Above points don't form a straight line.")
