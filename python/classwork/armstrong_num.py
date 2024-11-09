60ec555c96cf1736b19e44df341526af512621fe
dev <rajenendr2024@gmail.com>
2024-11-09 10:46:42 +0530

    armstrong num | perfect num

--- python/classwork/armstrong_num.py
+++ python/classwork/armstrong_num.py
@@ -1,18 +1,22 @@
+# Armstrong Number check
+
 n = int(input("Enter a Num: "))
-originalNum=n
+originalNum=n 
 i=0
 result=0
 
-while originalNum != 0:
-    originalNum = originalNum/10
+while originalNum != 0: 
+    originalNum = originalNum // 10
     i = i+1
+
+originalNum = n
 
 while(originalNum != 0):
     remainder = originalNum % 10
     result = result + (remainder ** i)
-    originalNum = originalNum/10
+    originalNum = originalNum // 10
 
 if(result == n):
     print("armstrong")
 else:
-    print("Not")
+    print("Not armstrong")
