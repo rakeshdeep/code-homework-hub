36d87166427b868fdc744e1af9c4e103ed266a6a
dev <rajenendr2024@gmail.com>
2024-11-22 10:56:19 +0530

    class work

--- c/classwork/22-11-2024/sum-naturalno-recursion.c
+++ c/classwork/22-11-2024/sum-naturalno-recursion.c
@@ -1,18 +1,21 @@
-//Wap to calculate the sum of n natural number using recursion
-#include<stdio.h>
-int sumnatural(int n){
-    if(n==0){
+// Wap to calculate the sum of n natural number using recursion
+#include <stdio.h>
+int sumnatural(int n)
+{
+    if (n == 0)
+    {
         return 0;
     }
-    else{
-        return n + sumnatural( n-1);
+    else
+    {
+        return n + sumnatural(n - 1);
     }
 }
-int main(){
+int main()
+{
     int n;
-printf("Entera positive number::");
-scanf("%d", &n);
-printf("Sum of first %d natural number is: %d\n",n,sumnatural(n));
-return 0;
-
+    printf("Entera positive number::");
+    scanf("%d", &n);
+    printf("Sum of first %d natural number is: %d\n", n, sumnatural(n));
+    return 0;
 }
