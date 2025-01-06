30f2373cb4225464d86f5014030b0712c310c070
dev <rajenendr2024@gmail.com>
2025-01-06 13:06:38 +0530

    ok

--- c/classwork/4-1-2025/copy_str_using_pointer.c
+++ c/classwork/4-1-2025/copy_str_using_pointer.c
@@ -7,11 +7,5 @@ void main(){
     gets(str1);
     p1 = str1;
     p2 = str2;
-    while(*p1 != '\0'){
-        *p2 = *p1;
-        p1++;
-        p2++;
-    }
-    *p2 = '\0';
-    printf("Copied string: %s\n", str2);
+    
 }
