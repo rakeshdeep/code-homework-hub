445773a5720d5c9626a8fff4d3d110ad7fa4c68e
dev <rajenendr2024@gmail.com>
2025-01-10 12:00:09 +0530

    structure

---
+++ c/classwork/structure_10-1-2025/struct_note.c
@@ -0,0 +1,25 @@
+// What is union?
+// union is a user-defined data type that allows you to store different data types in the same memory location.
+// You can define a union with many members, but only one member can contain a value at any given time.
+// Unions provide an efficient way of using the same memory location for multiple-purpose.
+// The memory occupied by a union will be large enough to hold the largest member of the union.
+// The memory occupied by a union will be large enough to hold the largest member of the union.
+// Unions provide an efficient way of using the same memory location for multiple-purpose.
+// Unions are used to define the structure of a variable in which you can store different data types.
+// Unions are similar to structures because they allow you to store data members of different data types.
+// what is struct?
+// A structure is a user-defined data type in C/C++. A structure creates a data type that can be used to group items of possibly different types into a single type.
+// Structure helps to construct a complex data type which is more meaningful.
+// A structure can contain any data type including array and another structure.
+// A structure can be initialized at the time of declaration.
+// syntax:
+// struct structure_name
+// {
+//     data_type member1;
+//     data_type member2;
+//     .
+//     .
+//     .
+//     data_type memberN;
+// }; 
+// struct structure_name object_name;
